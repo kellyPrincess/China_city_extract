537ddbfddf14a3bc1181de67ffb72724b234d736
kellyprincess <1019877285@qq.com>
2018-11-08 17:34:25 +0800

    add qu

--- city_town.py
+++ city_town.py
@@ -3,12 +3,14 @@
 import pandas as pd
 import requests
 import re
+import time
 import json
 import copy
+import warnings
+warnings.filterwarnings("ignore")
 
 
-if __name__ == '__main__':
-
+def prov_city_town():
     province = ["北京市",
                 "天津市",
                 "上海市",
@@ -64,8 +66,6 @@ if __name__ == '__main__':
     df["province2"] = list(map(lambda x: re.sub(r"省|市|回族自治区|壮族自治区|维吾尔自治区|自治区|特别行政区", "", x), df["province"]))
     df["city2"] = list(map(lambda x: re.sub(r"市|城区|自治州|自治县", "", x), df["city"]))
     df["town2"] = list(map(lambda x: re.sub(r"新区|区|市|自治县|县|镇|", "", x), df["town"]))  # 注意会出现单个字符串
-    # df.to_excel("chinatwon.xlsx")
-    # s = set([x[-1] for x in df['city']])
     df["qu"] = None
     for i in range(len(df)):
         if df["province2"][i] in ["山东", "江苏", "安徽", "江西", "浙江", "福建", "上海"]:
@@ -87,18 +87,65 @@ if __name__ == '__main__':
 
     p1 = df[["province", "qu"]]
     p2 = df[["province2", "qu"]]
-    p2.rename(columns={"province2": "province"}, inplace=True)
+    p1.rename(columns={"province": "pct"}, inplace=True)
+    p2.rename(columns={"province2": "pct"}, inplace=True)
     prov_df = pd.concat([p1, p2]).drop_duplicates()
+    prov_df.index = [i for i in range(len(prov_df))]
 
     c1 = df[["city", "qu"]]
     c2 = df[["city2", "qu"]]
-    c2.rename(columns={"city2": "city"}, inplace=True)
+    c1.rename(columns={"city": "pct"}, inplace=True)
+    c2.rename(columns={"city2": "pct"}, inplace=True)
     city_df = pd.concat([c1, c2]).drop_duplicates()
+    city_df.index = [i for i in range(len(city_df))]
 
     t1 = df[["town", "qu"]]
     t2 = df[["town2", "qu"]]
     t2 = t2[t2["town2"].map(len) > 1]  # 筛选出该列中长度大于1的值
-    t2.rename(columns={"town2": "town"}, inplace=True)
+    t1.rename(columns={"town": "pct"}, inplace=True)
+    t2.rename(columns={"town2": "pct"}, inplace=True)
     town_df = pd.concat([t1, t2]).drop_duplicates()
+    town_df.index = [i for i in range(len(town_df))]
 
-    print(town_df)
+    # pct = pd.concat([prov_df, city_df, town_df]).drop_duplicates()
+    # pct.index = [i for i in range(len(pct))]
+    return [prov_df, city_df, town_df]
+
+
+def read_hive():
+    df = prov_city_town()
+    df = df[0]
+
+    china_qu = open("name_qu.txt", "w", encoding="utf-8")
+    china_none = open("name_none.txt", "w", encoding="utf-8")
+    for i in range(1, 3):
+
+        cnt = 0
+        with open("ent_0{}".format(i), "r", encoding="utf-8") as f:
+            for line in f:
+                name = line.strip()
+                for j in range(len(df)):
+                    if str(df["pct"][j]) in name:
+                        qu = df["qu"][j]
+                        china_qu.write(name.strip() + "$" + qu.strip() + "\n")
+                        break
+                    else:
+                        qu = ''
+                        china_none.write(name.strip() + "$" + qu + "\n")
+
+                cnt += 1
+                if cnt % 50000 == 0:
+                    china_qu.flush()
+                    china_none.flush()
+                    print("{} cnt={}".format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), cnt))
+
+    china_qu.close()
+    china_none.close()
+    print("--over--")
+
+
+if __name__ == '__main__':
+
+    print(read_hive())
+
+
